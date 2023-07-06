50d4211a11bf3e8ebc30488dd8c434241c8dd46d
h2s <harshaal.shinde@gmail.com>
2023-07-06 07:54:30 +0530

    Find max operation done

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -9,7 +9,7 @@ const CoinAPI = require("../CoinAPI");
 class MongoBackend {
   constructor() {
     this.coinAPI = new CoinAPI();
-    this.mongoUrl = "mongodb://localhost:37017/maxcoin";
+    this.mongoUrl = "mongodb://localhost:30017/maxcoin";
     this.client = null;
     this.collection = null;
   }
@@ -43,7 +43,9 @@ class MongoBackend {
     return this.collection.insertMany(documents);
   }
 
-  async getMax() {}
+  async getMax() {
+    return this.collection.findOne({}, {sort: {value:-1}})
+  }
 
   async max() {
     console.info("Connection to MongoDB");
@@ -63,10 +65,21 @@ class MongoBackend {
 
     console.info(`Inserted ${insertResult.result.n} documents into MongoDB`);
 
+    console.info("Querying MongoDB");
+    console.time("mongodb-find");
+    const doc = await this.getMax();
+    console.timeEnd("mongodb-find");
+
+
     console.info("Disconnecting from MongoDB");
     console.time("mongodb-disconnect");
     await this.disconnect();
     console.timeEnd("mongodb-disconnect");
+
+    return{
+      date :doc.date,
+      value: doc.value
+    }
   }
 }
 
